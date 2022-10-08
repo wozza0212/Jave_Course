5c2d5de0fd02dcd35c0c1a9de10a94fd1ac15a6e
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-10-08 21:59:50 +0100

    Should add all joining strings files

---
+++ JavaMasterclass/HelloJava/Variables/JoiningStrings/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String sentence = "And his name was ";
+        String name = "Barry";
+        System.out.println(sentence + name);
+    }
+}
