c8c8a74f950be54fe9f0fb14071ec8a19a6768f3
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-10-08 22:02:13 +0100

    Should add rest

--- JavaMasterclass/HelloJava/Variables/JoiningStrings/src/Main.java
+++ JavaMasterclass/HelloJava/Variables/JoiningStrings/src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         String sentence = "And his name was ";
         String name = "Barry";
         System.out.println(sentence + name);
+
+        int points = 50;
+        String announcement = " Points for Gryffindor";
+        System.out.println(points + announcement);
     }
 }
