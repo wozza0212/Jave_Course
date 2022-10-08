5776612fbd41b1f883df609ddcb7e93a799e7471
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-10-08 20:43:20 +0100

    Challenge 1 done!

---
+++ JavaMasterclass/HelloJava/YourInitials/src/YourInitials.java
@@ -0,0 +1,9 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("TTTTTTT W       W");
+        System.out.println("   T    W       W");
+        System.out.println("   T    W   W   W");
+        System.out.println("   T    W   W   W");
+        System.out.println("   T    W W W W W");
+    }
+}
