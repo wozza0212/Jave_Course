6bdcd1fc9dd86ec809714c23f0e49b8588fe475a
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-10-09 15:22:36 +0100

    Adding decialms work

---
+++ JavaMasterclass/HelloJava/Variables/Decimals/src/Decimals.java
@@ -0,0 +1,9 @@
+public class Decimals {
+    public static void main(String[] args) {
+        //Decimals are stored in a double
+        double price = 3.99;
+        double percentage = 55.7;
+
+        System.out.println("In 2020, " + percentage + "% of the world had Covid");
+    }
+}
