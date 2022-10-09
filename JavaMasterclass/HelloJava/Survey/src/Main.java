e9a68e9d93bfaef4851992f21e7e819d2e861447
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-10-09 16:42:50 +0100

    Scanner stuff

--- JavaMasterclass/HelloJava/Survey/src/Main.java
+++ JavaMasterclass/HelloJava/Survey/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Good evening and welcome to the Quiz");
+        System.out.println("What is your name?");
+        String name = scan.nextLine();
+        System.out.println("How much do you spend on climbing gear?");
+        double spendClimb = scan.nextDouble();
+        System.out.println("Your name is " + name + " and you spend £" + spendClimb + " on climbing gear!");
     }
 }
