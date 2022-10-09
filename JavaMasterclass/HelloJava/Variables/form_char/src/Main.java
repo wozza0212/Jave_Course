a227d60ccd8fade563a6ac0c38fd3c59e0557d35
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-10-09 14:36:16 +0100

    Added workbook 2.1 to form_char

--- JavaMasterclass/HelloJava/Variables/form_char/src/Main.java
+++ JavaMasterclass/HelloJava/Variables/form_char/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = "Tom";
+        int age = 31;
+        String country = "England";
+        String sport = "Bouldering";
+        int hours = 10;
+        String subject = "Maths";
+        String game = "Risk";
+        char grade = 'A';
+
+        System.out.println("My name is " + name + " I'm " + age + " years old, and I'm from " + country);
+        System.out.println("My favourite sport is " + sport + ". I play for " + hours + " hours a day");
+        System.out.println("When I'm tired, I like to play " + game + ".");
+        System.out.println("In school, my favourite subject was " + subject + ", I scored an " + grade + ".");
+
     }
 }
