5890443fbf9d7476539b58cddfab84f32126a0bb
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-10-09 15:07:48 +0100

    Adding int long module

---
+++ JavaMasterclass/HelloJava/Variables/Whole_numbers/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Int uses up less memory than Long
+        int numOfFans = 5000;
+        System.out.println("There are " + numOfFans + " at the Quidditch games");
+
+        long globalPopulation = 7000000000000L;
+        System.out.println("The global population i think is around " + globalPopulation);
+    }
+}
