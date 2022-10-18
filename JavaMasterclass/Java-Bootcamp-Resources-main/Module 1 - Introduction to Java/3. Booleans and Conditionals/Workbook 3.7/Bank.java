8298947e9cfc8f7590bba7d0326f24628d4be14e
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-10-18 22:19:46 +0100

    Halfway through workbook 3.7

--- JavaMasterclass/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.7/Bank.java
+++ JavaMasterclass/Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.7/Bank.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
 
-public class Bank {
+public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);)%
         System.out.println("\n****ROYAL BANK OF JAVA****");
         System.out.println("Are you here to get a mortgage? (yes or no)");
+
         //Task 1 - Pick up the user's decision.
+        String decision = scan.nextLine();
 
 
         //Task 2 - Print this if the decision is "yes"
+        if (decision.equals('yes')) {
         System.out.println("\nGreat! In one line" +
             "\nHow much money do you have in your savings?" +
             "\nAnd, how much do you owe in credit card debt?");
-                // Task 3 - Pick up each value 
+
+        String savings = scan.nextLine();
+        String creditCardDebt = scan.nextLine();
+
+
+                // Task 3 - Pick up each value
+
             System.out.println("\nHow many years have you worked for?");
                // Task 4 - Pick up number of years
 
@@ -23,11 +32,15 @@ public class Bank {
                //       - Otherwise, don't give them a mortgage.          
                System.out.println("Congratulations <name> You have been approved!");
                System.out.println("Sorry, you are not eligible for a mortgage.");
+        }
 
-        //Task 2 - Print this if the decision was not "yes"
-        System.out.println("\nOK. Have a nice day!");
+        else {
+
+            //Task 2 - Print this if the decision was not "yes"
+            System.out.println("\nOK. Have a nice day!");
 
 
-        scan.close();
+            scan.close();
+        }
     }
 }
