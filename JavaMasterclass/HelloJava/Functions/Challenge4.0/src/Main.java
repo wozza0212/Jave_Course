146dd2132d2cd2eabf19f3f0b1ac707c6106c57b
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-10-19 21:24:06 +0100

    Added challenge4 not done

---
+++ JavaMasterclass/HelloJava/Functions/Challenge4.0/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Let's play Rock Paper Scissors.");
+        System.out.println("When I say 'shoot', Choose: rock, paper, or scissors.\n");
+        System.out.println("Are you ready? Write 'yes' if you are.");
+
+        String startGame = scan.nextLine();
+    }
+}
