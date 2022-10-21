a54745e16515a3b1697f5805cfc0ab3ddbbc8da7
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-10-21 22:21:46 +0100

    added workbook 4.3

---
+++ JavaMasterclass/HelloJava/Functions/Workbook4.3/src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        double noon = 77;          //temperature at noon in fahrenheit.
+        double evening = 61;       //temperature during the evening in fahrenheit
+        double midnight = 55;      //temperature at midnight in fahrenheit
+
+        double celsius = fahrenheitToCelsius(noon);
+        printTemperatures(noon, celsius);
+
+        //Task 3 - Call the printTemperatures function.
+
+
+
+    }
+
+
+    //Task 1: Make a function here. See the doc comments below.
+    public static double fahrenheitToCelsius(double temp) {
+        double celsius = (temp - 32) * 5 / 9;
+
+        return celsius;
+
+
+    }
+
+    public static void printTemperatures(double fahrenheit, double celsius) {
+        System.out.println("The temperature in fahrenheit is " + fahrenheit);
+        System.out.println("The temperature in celsius is " + celsius);
+
+
+    }
+
+    //Task 2: Make a function here. See the doc comments below.
+
+    /**
+     * Function name: printTemperatures – prints both temperature values.
+     * @param fahrenheit (double)
+     *
+     * Inside the function:
+     *  1. prints: F: <temperature in fahrenheit>.
+     *  2. prints: C: <temperature in celsius> .
+     */
+}
